ce6d118728e5ca2b9121496bef463030cb9dcf7a
autumnWindigo <fogeyfun@gmail.com>
2023-09-21 19:53:03 -0500

    added play_next and connection checks

--- src/mpd_connection.cpp
+++ src/mpd_connection.cpp
@@ -3,20 +3,28 @@
 #include <iostream>
 #include <mpd/connection.h>
 #include <mpd/error.h>
+#include <mpd/player.h>
 #include <ostream>
 
 using namespace std;
 
+enum Connection_state { connected, disconnected, error };
 class MPD_Connection {
     public:
     mpd_connection *conn;
+    Connection_state conn_state;
 
     // Default Constructor
     MPD_Connection() {
         conn = mpd_connection_new(NULL, 0, 0);
+        conn_state = connected;
+        check_error();
     }
+    void play_next();
 
-    bool is_error();
+    bool check_error();
+
+    void close_connection();
 };
 
 /**
@@ -26,14 +34,25 @@ class MPD_Connection {
  * @param mpd_connection within object
  * @return true if error, false otherwise
  */
-bool MPD_Connection::is_error() {
+bool MPD_Connection::check_error() {
     if (conn == NULL) {
         cerr << "Out of memory" << endl;
+        conn_state = disconnected;
         return true;
     }
     if (mpd_connection_get_error(conn) != MPD_ERROR_SUCCESS) {
         cerr << mpd_connection_get_error_message(conn) << endl;
+        conn_state = error;
         return true;
     }
     return false;
+}
+
+void MPD_Connection::play_next() {
+    check_error();
+    mpd_run_next(conn);
+}
+
+void MPD_Connection::close_connection() {
+
 }
