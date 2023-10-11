aaf0631cf24611ef49a7d0ecb2bb6cd202965868
autumnWindigo <fogeyfun@gmail.com>
2023-10-10 23:59:45 -0500

    Library documentation

--- src/library.cpp
+++ src/library.cpp
@@ -13,49 +13,42 @@ Library::Library() : playlist(), song_count(0) {};
 Library::~Library() {
 }
 
-// We need to allocate memory as we go for the library
-// Can't know the size it takes until it's already made.
-// loop through songs from MPD until null
-// and on each song allocate more memory to array
-
 void Library::prep_library() {
     MPD::Connection::instance()->check_error();
     mpd_send_list_queue_meta(MPD::Connection::instance()->get());
 }
 
-// Might be loading a playlist so can't use mpd_stats
-// This works just trust me
 void Library::load_library() {
     prep_library();
     MPD::Song *song = nullptr;
 
-    // TODO Check if playlist = NULL ever (that means heap failed return -1);
     while ((song = new MPD::Song)->is_safe() != false) {
         song_count++;
         playlist.push_back(*song);
     }
 }
 
-mpd_song *Library::find_song() {
+mpd_song *Library::find_song(std::string search) {
 // TODO Return song based on title
 // Must be sorted to match first
+    if (!is_sorted)
+        return nullptr;
+
+
     return nullptr;
 }
 
 void Library::sort(std::function<std::string(MPD::Song)> get_tag_value) {
-// TODO Sort based on input tag
     std::sort(playlist.begin(), playlist.end(),
             [get_tag_value](MPD::Song a, MPD::Song b)->int
             {
                 return boost::algorithm::to_lower_copy(get_tag_value(a))
                      < boost::algorithm::to_lower_copy(get_tag_value(b));
             });
+    is_sorted = true;
 }
 
-// This lets us do sort_by_custom?
 void Library::sort_by_date() {
-    // Passing Lambda into sort so we can pass any get function into sort
-    //   as long as it returns a string, so we can pass any function to it
     sort([](MPD::Song s)->std::string { return s.get_date(); });
     current_sort = MPD_TAG_DATE;
 }
--- src/library.h
+++ src/library.h
@@ -5,38 +5,100 @@
 #include <mpd/client.h>
 
 namespace MPD {
-// Stores information about song library
+
+/**
+ * @class Library
+ * @brief Holds set of mpd_songs. Full mpd library by default, can act as a subset or playlist.
+ *
+ */
 struct Library {
 public:
+    /**
+     * @brief Constructor for Library
+     */
     Library();
 
+    /**
+     * @brief Deconscructor for Library struct
+     */
     ~Library();
 
-    /* TODO
-       Quickly find song in library */
-    mpd_song *find_song();
+    /**
+     * @brief From the title of a song, returns pointer to song
+     *
+     * @param title Title of song
+     * @return Pointer to song
+     */
+    mpd_song *find_song(std::string title);
 
-    // Loads songs in MPD library into the playlist array
+    /**
+     * @brief Innitializes library with current system mpd library
+     */
     void load_library();
 
+    /**
+     * @brief Creates subset which contains songs that match current search
+     *
+     * @param search Current search for songs
+     * @return Subset matching search
+     */
+    Library find_songs(std::string search);
+
+    /**
+     * @brief returns total number of songs in library, -1 if empty.
+     *
+     * @return number of songs in Library
+     */
     int get_song_count();
 
-    // returns playlist
-    MPD::Song *get();
+    /**
+     * @brief Preforms on string tag using a getter which is passed as param
+     *
+     * @param get_tag_value Getter function which returns info from MPD::Song
+     */
+    void sort(std::function<std::string(MPD::Song)> get_tag_value);
 
+    /**
+     * @brief sorts current library by date
+     */
     void sort_by_date();
+
+    /**
+     * @brief sorts current library by title
+     */
     void sort_by_title();
+
+    /**
+     * @brief prints song titles in Library to standard out
+     */
     void print();
-    void sort(std::function<std::string(MPD::Song)>);
+
 
 private:
+    /**
+     * @brief Prepares Library to be innitialized
+     */
     void prep_library();
 
+    /**
+     * @brief Vector of MPD::Song(s) that acts as the main playlist of the library
+     */
     std::vector<MPD::Song> playlist;
 
-    // acts as upper bound for playlist array
-    int song_count;
+    /**
+     * @brief Number of songs in the Library
+     */
+    int song_count = 0;
 
+    /**
+     * @brief True if Library is currently sorted, false otherwise. Must be true for find_song(s).
+     *
+     */
+    bool is_sorted = false;
+
+    /**
+     * @brief Returns mpd_tag_type of current sort, NULL if not sorted or custom sort.
+     */
     mpd_tag_type current_sort;
 };
 }
