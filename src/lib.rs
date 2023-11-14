8e601bbe0285d5132b40c45578355c9c405facf2
jarmstrong22 <jadenarmstrong0722@gmail.com>
2023-11-14 17:18:57 -0500

    Comments to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+// import Neon and standard library modules
 use neon::prelude::*;
 use std::cell::RefCell;
 
+// import the m3p module
 mod m3p;
 
 type BoxedM3p = JsBox<RefCell<m3p::Connection>>;
 
-
+// Simple test function to return "hello node" string
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
@@ -18,7 +20,7 @@ fn m3p_connect(mut cx: FunctionContext) -> JsResult<JsString> {
     let con = cx.argument::<BoxedM3p>(0)?;
     let mut con = con.borrow_mut();
     con.init();
-    match &con.connection {
+    match &con.connection { // Check if the connection was successful
         Some(_) => Ok(cx.string("Connect to mpd at:".to_owned() + con.get_ip())),
         None => cx.throw_error("Failed to connect to mpd")
     }
@@ -34,9 +36,10 @@ fn m3p_new(mut cx: FunctionContext) -> JsResult<BoxedM3p> {
         connection: None,
     })))
 }
-
+// Main function: Neon module is initialized and functions are exported
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    // Export to node.js
     cx.export_function("hello", hello)?;
     cx.export_function("m3p_new", m3p_new)?;
     cx.export_function("m3p_connect", m3p_connect)?;
