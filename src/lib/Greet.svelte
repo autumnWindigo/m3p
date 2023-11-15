8f359bfc8445257703f9d17f9b5ea961ae7728c7
owen-mitch <78667282+owen-mitch@users.noreply.github.com>
2023-11-15 17:51:06 -0500

    Merge branch 'frontend' into main

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { listen } from '@tauri-apps/api/event'
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    let temp = '127.0.0.1'
+    let ip = ''
+    let dumb = ''
+
+    console.log("hi")
+    async function sendIP() {
+        let response = invoke('send_ip', { temp })
+        await listen('rs2js', (event) => { console.log(event.payload) })
+
+    }
+
+</script>
+
+<div>
+    <input id="greet-input" placeholder="Enter an ip..." bind:value="{dumb}" />
+    <button on:click="{sendIP}">Greet</button>
+    <p>{ip}</p>
+</div>
