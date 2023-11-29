451ab3062a9eb340c86ac18db69382f67cb87f51
jarmstrong22 <jadenarmstrong0722@gmail.com>
2023-11-29 15:31:59 -0500

    Fix to make checkbox appear visible

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,93 +1,95 @@
 <!-- Settings.svelte -->
 <script>
-    let userName = "John Doe";
-    let isAutoPlayEnabled = false;
+	let userName = "John Doe";
+	let isAutoPlayEnabled = false;
 	let yes = false;
 
-    function toggleAutoPlay() {
-        isAutoPlayEnabled = !isAutoPlayEnabled;
-        // Add logic to handle auto-play state change
-        console.log("Auto-play state changed:", isAutoPlayEnabled);
-    }
+	function toggleAutoPlay() {
+	isAutoPlayEnabled = !isAutoPlayEnabled;
+	// Add logic to handle auto-play state change
+	console.log("Auto-play state changed:", isAutoPlayEnabled);
+	}
 </script>
 
 <main>
 	<h1>{userName}'s Settings</h1>
 	<h2>Auto-play</h2>
-	<label>
-		<input type="checkbox" bind:checked={isAutoPlayEnabled}/>
+	<label class="auto-play-toggle">
+		<input type="checkbox" bind:checked={"isAutoPlayEnabled"}/>
 		<span class="slider"></span>
 	</label>
 	<h3>Subscribe to email</h3>
-	<label>
-		<input type="checkbox" bind:checked={yes} />
+	<label class="subscribe-label">
+		<input type="checkbox" bind:checked={"yes"} />
+		Subscribe
 	</label>
 	{#if yes}
 	<p>Thank you. We will bombard your inbox and sell your personal details.</p>
 	{:else}
 	<p>If you're not paying, you're the product.</p>
 	{/if}
-	<button disabled={!yes}> Subscribe </button>
-	
+	<button disabled={!"yes"}>Subscribe</button>
+
 	<p>Change color</p>
-	<a href="/">About</a>
+	<a href="/">About us</a>
 	<a href="/">Back</a>
-	
 </main>
-
-
 <style>
-    /* Add styling for the settings page */
-    .settings-container {
-        padding: 20px;
-    }
+	/* Add styling for the settings page */
+	main {
+	padding: 20px;
+	}
 
-    .auto-play-toggle {
-        display: flex;
-        align-items: center;
-        margin-top: 20px;
-    }
+	.auto-play-toggle {
+	display: flex;
+	align-items: center;
+	margin-top: 20px;
+	}
 
-    label {
-        position: relative;
-        display: inline-block;
-        width: 60px;
-        height: 34px;
-    }
+	.subscribe-label {
+	display: flex;
+	align-items: center;
+	margin-top: 10px;
+	cursor: pointer;
+	}
 
-    input {
-        display: none;
-    }
+	.subscribe-label input {
+	margin-right: 10px;
+	}
 
-    .slider {
-        position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #ccc;
-        border-radius: 34px;
-        transition: .4s;
-    }
+	input {
+	display: none;
+	}
 
-    input:checked + .slider {
-        background-color: #2196F3;
-    }
+	.slider {
+	position: absolute;
+	cursor: pointer;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: #ccc;
+	border-radius: 34px;
+	transition: .4s;
+	}
 
-    .slider:before {
-        position: absolute;
-        content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        border-radius: 50%;
-        transition: .4s;
-    }
+	input:checked + .slider {
+	background-color: #2196F3;
+	}
 
-    input:checked + .slider:before {
-        transform: translateX(26px);
-    }
+	.slider:before {
+	position: absolute;
+	content: "";
+	height: 26px;
+	width: 26px;
+	left: 4px;
+	bottom: 4px;
+	background-color: white;
+	border-radius: 50%;
+	transition: .4s;
+	}
+
+	input:checked + .slider:before {
+	transform: translateX(26px);
+	}
 </style>
