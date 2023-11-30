98cb14f3fdac7106ea2d0de1ed24a70556fb1e2c
jarmstrong22 <jadenarmstrong0722@gmail.com>
2023-11-30 12:23:42 -0500

    attempt to add dark mode function

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,102 +1,125 @@
 <!-- Settings +page.svelte -->
 <script>
+	import { onMount } from 'svelte';
+	import { colorMode } from './colorMode.js';
+	
 	let userName = "John Doe";
 	let isAutoPlayEnabled = false;
-	let yes = false;
-
+	
 	function toggleAutoPlay() {
 		isAutoPlayEnabled = !isAutoPlayEnabled;
 		// Add logic to handle auto-play state change
 		console.log("Auto-play state changed:", isAutoPlayEnabled);
 	}
+	
+	function toggleDarkMode() {
+		colorMode.update(mode=> (mode === 'light' ? 'dark' : 'light'));
+		console.log("Dark mode state changed:", $colorMode);
+	}
+	
+	$: updateBackgroundColor();
+	
+	function updateBackgroundColor() {
+		const body = document.body;
+		if ($colorMode == 'dark'){
+			body.style.backgroundColor = '#333';
+		} else {
+			body.style.backgroundColor = 'ivory';
+		}
+	}
+	
+	onMount(() => {
+		updateBackgroundColor();
+	});
+	
 </script>
 
 <main>
 	<h1>{userName}'s Settings</h1>
 
-	<p>Auto-play</p>
-	<label class="auto-play-toggle">
-		<input type="checkbox" bind:checked={"isAutoPlayEnabled"}/>
-		<span class = "slider"></span>
-	</label>
-
-	<p>Subscribe to Email</p>
-    <label class="subscribe-label">
-        <input type="checkbox" bind:checked={yes} />
-       Subscribe
-    </label>
-
-    {#if yes}
-        <p>Thank you. We will bombard your inbox and sell your personal details.</p>
-    {:else}
-        <p>If you're not paying, you're the product.</p>
-    {/if}
-
-    <button disabled={!yes}>Subscribe</button>
+	<div class="auto-play-toggle">
+		<p>Auto-play</p>
+		<label>
+			<input type="checkbox" bind:checked={isAutoPlayEnabled}/>
+			<span class = "slider"></span>
+		</label>
+	</div>
 
     <p class="section-heading">Change Color</p>
+	<button on:click={toggleDarkMode} class="color-mode-button">
+		Toggle Dark Mode: {$colorMode === 'dark' ? 'On' : 'Off'}
+	</button>
 
     <a href="/">About us</a>
+	<a href="/">Listening history</a> /*at some point we can import an array that adds on any song listened to in the session*/
     <a href="/">Back</a>
 </main>
 <style>
 	main {
-		padding: 20px;
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 	}
 
 	.auto-play-toggle {
-		display: flex;
-		align-items: center;
-		margin-top: 10px;
-		margin-bottom: 20px;
+	display: flex;
+	align-items: center;
+	margin-top: 20px;
 	}
-	
-	.subscribe-label {
-		display: flex;
-		align-items:center;
-		margin-top: 10px;
-		cursor: pointer;
-		color: red;
-		font-weight: bold;
-	}
-	
-	.subscribe-label input {
-		margin-right: 10px;
+
+	label {
+	position: relative;
+	display: inline-block;
+	width: 60px;
+	height: 34px;
 	}
 
 	input {
-		display: none;
+	display: none;
 	}
-	
+
 	.slider {
-		position: absolute;
-		cursor: pointer;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background-color: #ccc;
-		border-radius: 34px;
-		transition: .4s;
+	position: absolute;
+	cursor: pointer;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	width: 60px;
+	height: 34px;
+	background-color: #ccc;
+	border-radius: 34px;
+	transition: .4s;
 	}
 
 	input:checked + .slider {
-		background-color: #2196F3;
+	background-color: #2196F3;
 	}
 
 	.slider:before {
-		position: absolute;
-		content: "";
-		height: 26px;
-		width: 26px;
-		left: 4px;
-		bottom: 4px;
-		background-color: white;
-		border-radius: 50%;
-		transition: .4s;
+	position: absolute;
+	content: "";
+	height: 26px;
+	width: 26px;
+	left: 4px;
+	bottom: 4px;
+	background-color: white;
+	border-radius: 50%;
+	transition: .4s;
 	}
 
 	input:checked + .slider:before {
-		transform: translateX(26px);
+	transform: translateX(26px);
+	}
+
+	.color-mode-button {
+	margin-top: 10px;
+	padding: 10px;
+	background-color: #2196F3; /* Set a background color for better visibility */
+	color: #fff; /* Set text color */
+	cursor: pointer;
+	border: none;
+	border-radius: 4px;
 	}
 </style>
