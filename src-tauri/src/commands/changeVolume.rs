e9afb70ebf09caf7df0cfdd01871a7638da158d5
Michael Black <jb747216@sju.edu>
2023-12-07 17:48:49 -0500

    Corrected some of the commands

--- src-tauri/src/commands/changeVolume.rs
+++ src-tauri/src/commands/changeVolume.rs
@@ -6,5 +6,5 @@ use async_mpd::{MpdClient, Status};
 pub async fn changeVolume(newVolume: f32) -> null{
     let mut client = MpdClient::new();
     client.connect("127.0.0.1:6600").await.expect("cannot connect to client");
-    client.volume=newVolume.into()?
+    client.volume=newVolume.into()
 }
