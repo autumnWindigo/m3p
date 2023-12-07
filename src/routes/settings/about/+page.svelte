3f96a47cbb7068fe98162a241b43fdc8c5a819dc
jarmstrong22 <jadenarmstrong0722@gmail.com>
2023-12-07 18:07:51 -0500

    fix to about page

--- src/routes/settings/about/+page.svelte
+++ src/routes/settings/about/+page.svelte
@@ -4,34 +4,102 @@
 		<h2>
 			<strong>Our Story</strong>
 		</h2>
-		<p>
-			<strong>Your Music, Your Way:</strong>Welcome to Minimal Multi-platform Music Player (M3P),where music meets customization and control.
-			At its core, M3P is more than just a music player; it's a versatile and hackable platform designed
-			to adapt to your unique preferences.<br> <strong>The Foundation: An Extensible Music Player.</strong>
-			M3P starts as a simple list of songs in a folder, providing the fundamental ability to play your music. However, what sets it apart is
-			its hackable configuration file, enabling you to transform the player into something far beyond the basics. <br></br>
-			<strong>Multiplatform Magic with Music Player Daemon (MPD)</strong>
-			Embracing the power of Music Player Daemon (MPD), M3P becomes a multiplatform marvel. MPD facilitates music playback across various sound servers,
-			making M3P compatible with Linux distributions, Android, Apple, and Windows. With playlist capabilities and essential music player features, MPD lays
-			the foundation for M3P's expansive capabilities.<br></br>
-			<strong>Customization: Tailor Your Music Experience</strong>M3P stands out in the crowded music player landscape due to its emphasis on customization. Unlike other services, M3P provides a more minimalistic 
-			base, allowing users to shape their music player according to their desires. In an era where personalization is key, M3P empowers users to infuse 
-			their personality into their music experience.<br></br>
-			<strong>Tagging Mastery: Unleash the Power of Information</strong>Tagging is the unsung hero of M3P, offering unparalleled control over your music 
-			library. Automate tagging based on parameters you set, transforming messy data into organized information. From cleaning up artist names to trimming 
-			unnecessary content, M3P ensures that your music library reflects your preferences.<br></br>
-			<strong>Artwork Awesomeness: Visualize Your Music</strong>Visualizing your music goes beyond desktop experiences. M3P leverages tags and metadata 
-			to manage album artwork seamlessly. For Android users, the "ALBUM" tag becomes the key to unique and personalized album art. Navigate the visual
-			the landscape of your music effortlessly, with each song having its distinct image.<br></br>
-			<strong>Seamless Transition: From Cloud to Local</strong>Moving your library from a cloud service to a local program has never been easier. M3P 
-			simplifies the transition by allowing you to link a public Spotify playlist, effortlessly finding alternative options for easy song downloads. 
-			Say goodbye to the hassle of moving an entire library; now, it's as simple as pasting a link and clicking a few buttons.<br></br>
-			<strong>Audio Visualizer: A Symphony of Creativity</strong>Unleash your creativity with M3P's audio visualizer. While creating a hackable 
-			visualizer is a challenging feat, M3P envisions a platform where creative tty visualizers seamlessly interface with other programs. Imagine 
-			a unique, customizable audio-visual experience that truly complements your music journey.<br></br>
-			M3P is not just a music player; it's a canvas for your musical expression. Dive into the world of M3P, where your music takes center stage, 
-			tailored to your preferences, and brought to life with unrivaled customization.
-		</p>
+		<section id ="title">
+			<p>
+				<strong>Your Music, Your Way:</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				Welcome to Minimal Multi-platform Music Player (M3P),where music meets customization and control.
+				At its core, M3P is more than just a music player; it's a versatile and hackable platform designed
+				to adapt to your unique preferences.
+			</p>
+		</section>
+		<section id ="title">
+			<p>
+				<strong>The Foundation: An Extensible Music Player</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				M3P starts as a simple list of songs in a folder, providing the fundamental ability to play your music. However, what sets it apart is
+				its hackable configuration file, enabling you to transform the player into something far beyond the basics.
+			</p>
+		</section>
+		<section id = "title">
+			<strong>
+				<p>Multiplatform Magic with Music Player Daemon (MPD)</p>
+			</strong>
+		</section>
+		<section id ="summary">
+			<p>
+				Embracing the power of Music Player Daemon (MPD), M3P becomes a multiplatform marvel. MPD facilitates music playback across various sound 
+				servers,making M3P compatible with Linux distributions, Android, Apple, and Windows. With playlist capabilities and essential music player 
+				features, MPD lays the foundation for M3P's expansive capabilities.
+			</p>
+		</section>
+		<section id ="title">
+			<p>
+				<strong>Customization: Tailor Your Music Experience</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				M3P stands out in the crowded music player landscape due to its emphasis on customization. Unlike other services, M3P provides a more minimalistic
+				base, allowing users to shape their music player according to their desires. In an era where personalization is key, M3P empowers users to infuse
+				their personality into their music experience.
+			</p>
+		</section>
+		<section id ="title">
+			<p>
+				<strong>Tagging Mastery: Unleash the Power of Information</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				tagging is the unsung hero of M3P, offering unparalleled control over your music
+				library. Automate tagging based on parameters you set, transforming messy data into organized information. From cleaning up artist names to trimming
+				unnecessary content, M3P ensures that your music library reflects your preferences.
+			</p>
+		</section>
+		<section id = "title">
+			<p>
+				<strong>Artwork Awesomeness: Visualize Your Music</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				Visualizing your music goes beyond desktop experiences. M3P leverages tags and metadata
+				to manage album artwork seamlessly. For Android users, the "ALBUM" tag becomes the key to unique and personalized album art. Navigate the visual
+				the landscape of your music effortlessly, with each song having its distinct image.
+			</p>
+		</section>
+		<section id ="title">
+			<p>
+				<strong>Seamless Transition: From Cloud to Local</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				Moving your library from a cloud service to a local program has never been easier. M3P
+				simplifies the transition by allowing you to link a public Spotify playlist, effortlessly finding alternative options for easy song downloads.
+				Say goodbye to the hassle of moving an entire library; now, it's as simple as pasting a link and clicking a few buttons.
+			</p>
+		</section>
+		<section id ="title">
+			<p>
+				<strong>Audio Visualizer: A Symphony of Creativity</strong>
+			</p>
+		</section>
+		<section id ="summary">
+			<p>
+				Unleash your creativity with M3P's audio visualizer. While creating a hackable
+				visualizer is a challenging feat, M3P envisions a platform where creative tty visualizers seamlessly interface with other programs. Imagine
+				a unique, customizable audio-visual experience that truly complements your music journey. | M3P is not just a music player; it's a canvas for your musical expression. Dive into the world of M3P, where your music takes center stage,
+				tailored to your preferences, and brought to life with unrivaled customization.
+			</p>
+		</section>
 	</section>
 
 	<section id="customer-service">
@@ -52,6 +120,14 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+	
+	#title {
+		margin-bottom: .3em;
+		
+	}
+	#summary {
+		margin-bottom: 2em;
 	}
 	
 	#about {
