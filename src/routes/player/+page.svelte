a521dbaab89e08a5d7c5fbcd33f6e01f12338742
owen-mitch <odawgftw@gmail.com>
2023-12-12 17:00:06 -0500

    Houston, we have audio playback

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,40 +1,81 @@
 <script>
-    import MainPlayerContent from "./MainPlayerContent.svelte";
-    import Playbar from "./Playbar.svelte";
+  import Playbar from "./Playbar.svelte";
+  import { writable, get } from "svelte/store";
+  import { onMount } from "svelte";
 
-    let songs = [
-        { artist: 'Artist 1', title: 'Song 1', album: 'Album 1' },
-        { artist: 'Artist 2', title: 'Song 2', album: 'Album 2' },
-        { artist: 'Artist 3', title: 'Song 3', album: 'Album 3' },
-    ];
+  // Create a Svelte store for the songs array
+  const songsStore = writable([]);
+
+  let selectedSong = null;
+
+  const handleFileSelect = async (event) => {
+    const newSongs = [];
+
+    for (const file of event.target.files) {
+      if (file.type.startsWith("audio/")) {
+        const arrayBuffer = await file.arrayBuffer();
+        const audioData = new Uint8Array(arrayBuffer);
+        newSongs.push({
+          title: file.name,
+          artist: "Unknown Artist",
+          album: "Unknown Album",
+          audioData,
+        });
+      }
+    }
+
+    // Update the songs array in the store
+    songsStore.set(newSongs);
+    console.log("Files processed:", newSongs);
+  };
+
+  // Subscribe to changes in the songs store
+  $: {
+    const songs = get(songsStore);
+    console.log("Songs updated:", songs);
+  }
+
+  function playSong(song) {
+    selectedSong = song;
+  }
+
+  // Use onMount to trigger the reactivity when songs change
+  onMount(() => {
+    $: console.log("Songs updated:", get(songsStore));
+  });
 </script>
 
 <div>
-    <div id="player-background">
-        <div id="main-menu">
-            <h2>Main Menu</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Artist</th>
-                        <th>Song</th>
-                        <th>Album</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each songs as { artist, title, album }}
-                        <tr>
-                            <td>{artist}</td>
-                            <td>{title}</td>
-                            <td>{album}</td>
-                        </tr>
-                    {/each}
-                </tbody>
-            </table>
-        </div>
-        <a id="back" href="/">back</a>
-        <Playbar />
+  <div id="player-background">
+    <div id="main-menu">
+      <h2>Main Menu</h2>
+      <input type="file" webkitdirectory directory multiple on:change={handleFileSelect} />
+      <table>
+        <thead>
+          <tr>
+            <th>Artist</th>
+            <th>Song</th>
+            <th>Album</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each $songsStore as { artist, title, album, audioData }}
+          <tr on:click={() => playSong({ artist, title, album, audioData })}>
+            <td>{artist}</td>
+            <td>{title}</td>
+            <td>{album}</td>
+          </tr>
+          {/each}
+        </tbody>
+      </table>
     </div>
+    <a id="back" href="/">
+      back
+    </a>
+    {#if selectedSong}
+    <Playbar {selectedSong} {songsStore} />
+    {/if}
+  </div>
 </div>
 
 <style>
